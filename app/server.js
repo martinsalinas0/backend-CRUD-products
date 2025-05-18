69169f7e029f8bf4a3c38a301842b78a1ffb26eb
martinsalinas0 <martinlsalinas@pm.me>
2025-05-17 23:32:22 -0500

    added all endpoints and are functional

--- app/server.js
+++ app/server.js
@@ -71,6 +71,86 @@ app.delete("/api/product/:productId", async (req, res) => {
   }
 });
 
+//GET /products/productid/review
+//GETs the all reviews for a certain product (4 at a time)
+app.get("/api/product/:productId/reviews", async (req, res) => {
+  try {
+    const { productId } = req.params;
+
+    const product = await Product.findById(productId);
+
+    //if no product
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    //if yes prodcut but no reviews
+    if (product.reviews.length === 0) {
+      return res.status(404).json({ message: "No reviews available" });
+    }
+    res.status(200).json(product.reviews);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/api/product/:productId/reviews", async (req, res) => {
+  try {
+    const { productId } = req.params;
+
+    const { User, Stars, reviewText } = req.body;
+
+    if (!User || !Stars || !reviewText) {
+      return res.status(400).json({
+        message: "username, starts, and review description are required.",
+      });
+    }
+
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    const newReview = {
+      User,
+      Stars,
+      reviewText,
+      datePosted: new Date(),
+    };
+
+    product.reviews.push(newReview);
+    await product.save();
+
+    res.status(201).json({ message: "Review added", review: newReview });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//DELETE /product/productId/review
+//deletes a review from the product
+app.delete("/api/reviews/:reviewId", async (req, res) => {
+  try {
+    const { reviewId } = req.params;
+
+    const product = await Product.findOne({ "reviews._id": reviewId });
+    if (!product) {
+      return res.status(404).json({ message: "Review not found" });
+    }
+
+    product.reviews = product.reviews.filter(
+      (review) => review._id.toString() !== reviewId
+    );
+
+    await product.save();
+
+    res.status(200).json({ message: "review has been deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 mongoose
   .connect("mongodb://localhost/products")
   .then(() => {
